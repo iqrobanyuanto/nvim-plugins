92da09f224cfced52f6ddabf40fbc4b8ecf38ee8
Xeloreon <iqrobanyuanto12345@gmail.com>
2025-12-27 19:01:46 +0700

    refine lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,48 @@
-return{
- {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons'
-  },
-  opts = {
-    options = {
-      component_separators = { left = "|", right = "|" },
-      -- section_separators = { left = "", right = "" },
-      icons_enabled = false,
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
-    sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch'},
-      lualine_c = {'%f'},
-      lualine_x = {'hostname'},
-      lualine_z = {'location'},
-    },
+    config = function()
+      local function get_git_info()
+        -- 1. Get the current local branch
+        local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null"):gsub("\n", "")
+
+        -- If not in a git repo, branch will be empty
+        if branch == "" then
+          return nil
+        end
+
+        -- 2. Get the remote tracking branch (e.g., origin/main)
+        local remote = vim.fn.system("git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null"):gsub("\n", "")
+
+        -- 3. Return a formatted string or separate values
+        if remote ~= "" then
+          return string.format(" %s...%s ", branch, remote)
+        else
+          return string.format( "%s (no remote) ", branch)
+        end
+      end
+
+      -- Example: Print to the Neovim command line
+      print(get_git_info())
+
+      require("lualine").setup({
+        options = {
+          theme = "powerline_dark",
+          -- component_separators = { left = "|", right = "|" },
+          -- section_separators = { left = "", right = "" },
+          icons_enabled = true,
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { get_git_info },
+          lualine_c = { "%f" },
+          lualine_y = { "searchcount" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
   },
- }
 }
